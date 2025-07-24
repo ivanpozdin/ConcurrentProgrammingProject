b50b517c70b73517384f778c46d2907c9dfc4815
ivanpozdin <ivanllpozdin@gmail.com>
2025-07-24 15:37:07 +0200

    add MonitorQueue

---
+++ src/main/java/com/pseuco/cp25/simulation/rocket/MonitorQueue.java
@@ -0,0 +1,45 @@
+package com.pseuco.cp25.simulation.rocket;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * A simple queue supporting only enqueuing(adding) and dequeuing(removing) elements.
+ *
+ * @param <T>
+ */
+public class MonitorQueue<T> {
+    private final Queue<T> queue;
+
+    /**
+     * Creates an empty queue.
+     */
+    public MonitorQueue() {
+        queue = new ArrayDeque<>();
+    }
+
+    /**
+     * Enqueues element.
+     * @param e Element to queue.
+     */
+    public synchronized void enqueue(T e) {
+        queue.add(e);
+        this.notifyAll();
+    }
+
+    /**
+     * Returns the head of the queue and removes it.
+     * @return head of the queue.
+     * @throws InterruptedException
+     */
+    public synchronized T dequeue() throws InterruptedException {
+        while (queue.isEmpty()) try {
+            this.wait();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException("Thread was interrupted unexpectedly.", e);
+        }
+        return queue.remove();
+    }
+
+}
