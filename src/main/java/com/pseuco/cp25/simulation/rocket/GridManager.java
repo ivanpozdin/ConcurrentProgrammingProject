496f6d597002d3d99ea0ee31d24b59c59501b930
Tigran <timi00004@stud.uni-saarland.de>
2025-07-28 03:22:40 +0200

    Starship implementation including the GridManager class that optimizes patch initialization

---
+++ src/main/java/com/pseuco/cp25/simulation/rocket/GridManager.java
@@ -0,0 +1,172 @@
+package com.pseuco.cp25.simulation.rocket;
+
+import com.pseuco.cp25.model.*;
+
+import java.util.*;
+
+public class GridManager {
+    private final Rectangle grid;
+    private final Map<XY, Integer> cellToComponent = new HashMap<>();
+    private final List<Set<XY>> components = new ArrayList<>();
+    private final Set<Integer> emptyComponents;
+
+    public GridManager(Scenario scenario) {
+        this.grid = scenario.getGrid();
+        int width = scenario.getGridSize().getX();
+        int height = scenario.getGridSize().getY();
+
+        // Initialize grid matrix without obstacles
+        boolean[][] free = new boolean[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                free[x][y] = true;
+            }
+        }
+
+        // Mark all obstacles in out grid
+        for (Rectangle rect : scenario.getObstacles()) {
+            for (XY cell : rect) {
+                free[cell.getX()][cell.getY()] = false;
+            }
+        }
+
+        // Compute grid's components
+        boolean[][] visited = new boolean[width][height];
+        int currentId = 0;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (free[x][y] && !visited[x][y]) {
+                    Set<XY> component = new HashSet<>();
+                    gridBFS(new XY(x, y), free, visited, component, currentId);
+                    components.add(component);
+                    currentId++;
+                }
+            }
+        }
+
+        // Find empty components
+        emptyComponents = findEmptyComponentIds(scenario.getPopulation());
+    }
+
+    /**
+     * BFS pass over the grid starting from a given point that forms a component
+     */
+    private void gridBFS(XY start, boolean[][] free, boolean[][] visited, Set<XY> component, int componentId) {
+        Deque<XY> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited[start.getX()][start.getY()] = true;
+        cellToComponent.put(start, componentId);
+
+        while (!queue.isEmpty()) {
+            XY curr = queue.poll();
+            component.add(curr);
+            for (Direction dir : Direction.values()) {
+                if (dir == Direction.NONE) continue;
+
+                XY neighbor = new XY(
+                        dir.getVector().getX() + curr.getX(),
+                        dir.getVector().getY() + curr.getY()
+                );
+                int nx = neighbor.getX();
+                int ny = neighbor.getY();
+
+                if (grid.contains(neighbor) && free[nx][ny] && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    cellToComponent.put(neighbor, componentId);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    /**
+     * Given a population calculates a set of empty components' IDs.
+     * @return set of empty components' IDs
+     */
+    private Set<Integer> findEmptyComponentIds(List<PersonInfo> population) {
+        boolean[] hasPerson = new boolean[components.size()];
+
+        for (PersonInfo p : population) {
+            int compId = getComponentId(p.getPosition());
+            if (compId >= 0) {
+                hasPerson[compId] = true;
+            }
+        }
+
+        Set<Integer> emptyIds = new HashSet<>();
+        for (int i = 0; i < hasPerson.length; i++) {
+            if (!hasPerson[i]) {
+                emptyIds.add(i);
+            }
+        }
+
+        return emptyIds;
+    }
+
+    /**
+     * An optimized version of the method of the same name from common.Utils class
+     */
+    public boolean mayPropagateFrom(
+            final Scenario scenario,
+            final Rectangle source,
+            final Rectangle target) {
+        final Set<XY> region = new HashSet<>();
+        final Set<XY> frontier = new HashSet<>();
+        for (final XY targetCell : target) {
+            if (!scenario.onObstacle(targetCell)) {
+                frontier.add(targetCell);
+            }
+        }
+        final int infectionRadius = scenario.getParameters().getInfectionRadius();
+        while (!frontier.isEmpty()) {
+            final XY cell = frontier.iterator().next();
+            frontier.remove(cell);
+            region.add(cell);
+            // Process the neighbors of the current cell
+            if (processNeighbors(scenario, cell, infectionRadius, frontier, region, source)) {
+                return true;  // If any neighbor is part of the source, return true immediately
+            }
+        }
+
+        // If no reachable source cells found, return false
+        return false;
+    }
+
+    private boolean processNeighbors(
+            Scenario scenario,
+            XY cell,
+            int infectionRadius,
+            Set<XY> frontier,
+            Set<XY> region,
+            Rectangle source) {
+        for (int deltaX = -infectionRadius; deltaX <= infectionRadius; deltaX++) {
+            for (int deltaY = -infectionRadius; deltaY <= infectionRadius; deltaY++) {
+                if (Math.abs(deltaX) + Math.abs(deltaY) <= infectionRadius
+                        || (Math.abs(deltaX) <= 1 && Math.abs(deltaY) <= 1)) {
+                    final XY neighbor = cell.add(deltaX, deltaY);
+                    if (!region.contains(neighbor)
+                            && scenario.getGrid().contains(neighbor)
+                            && !scenario.onObstacle(neighbor)
+                            // Additionally check if neighbour ends up in an empty component
+                            && !emptyComponents.contains(getComponentId(neighbor))) {
+                        // Return immediately if rectangles have connected
+                        if (source.contains(neighbor)) {
+                            return true;
+                        }
+                        frontier.add(neighbor);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns component ID that the provided cell is contained in.
+     * If a cell does not belong to a component returns -1.
+     * @return component ID or -1 on no component ownership
+     */
+    public int getComponentId(XY cell) {
+        return cellToComponent.getOrDefault(cell, -1);
+    }
+}
--- src/main/java/com/pseuco/cp25/simulation/rocket/Starship.java
+++ src/main/java/com/pseuco/cp25/simulation/rocket/Starship.java
@@ -1,8 +1,12 @@
 package com.pseuco.cp25.simulation.rocket;
 
-import com.pseuco.cp25.model.Output;
-import com.pseuco.cp25.model.Scenario;
+import com.pseuco.cp25.model.*;
+import com.pseuco.cp25.simulation.common.Person;
 import com.pseuco.cp25.simulation.common.Simulation;
+import com.pseuco.cp25.validator.DummyValidator;
+import com.pseuco.cp25.validator.Validator;
+
+import java.util.*;
 
 /**
  * The implementation of assignment 2 should go here.
@@ -12,7 +16,21 @@ import com.pseuco.cp25.simulation.common.Simulation;
  * </p>
  */
 public class Starship implements Simulation {
+    private final int cycleDuration;
+    private final Scenario scenario;
+    private final int padding;
+    private final List<Patch> patches = new ArrayList<>();
+    private final List<Person> initialPopulation = new ArrayList<>();
 
+    // Statistics-related fields
+    private final int statsLength;
+    private final Map<String, List<Statistics>> totalStatistics = new HashMap<>();
+    private final List<TraceEntry> totalTrace;
+    private final List<MonitorQueue<OutputEntry>> outputQueues;
+
+    private final List<Thread> threads = new ArrayList<>();
+    private final GridManager gridManager;
+    Validator validator;
     /**
      * Constructs a starship with the given parameters.
      *
@@ -24,18 +42,217 @@ public class Starship implements Simulation {
      * @param padding Optional padding.
      */
     public Starship(Scenario scenario, int padding) {
-        // your concurrent implementation of assignment 2 goes here
-        // you may ignore the `padding` parameter.
-        throw new RuntimeException("not implemented");
+        this.scenario = scenario;
+        this.statsLength = scenario.getTicks() + 1;
+        outputQueues = new ArrayList<>(scenario.getNumberOfPatches());
+
+        int tempPadding = padding;
+        int tempCycle = getTicks(tempPadding);
+        while (tempCycle == 0) {
+            tempPadding++;
+            tempCycle = getTicks(tempPadding);
+        }
+        this.cycleDuration = tempCycle;
+        this.padding = tempPadding;
+        this.gridManager = new GridManager(scenario);
+        this.validator = new DummyValidator();
+
+        populate();
+        createPatches();
+        createPaddings();
+        initializeStatistics();
+
+        if (scenario.getTrace()) {
+            totalTrace = new ArrayList<>();
+        } else {
+            totalTrace = new ArrayList<>(statsLength);
+        }
+    }
+
+    /**
+     * Calculates cycle's duration.
+     *
+     * @return Cycle duration.
+     */
+    private int getTicks(int padding) {
+        int t = 1;
+        while (padding >= movementUncertainty(t) + infectionUncertainty(t)) {
+            t++;
+        }
+        return t - 1;
+    }
+
+    private int movementUncertainty(int ticks) {
+        return 2 * ticks;
+    }
+
+    private int infectionUncertainty(int ticks) {
+        int incubationTime = scenario.getParameters().getIncubationTime();
+        int radius = scenario.getParameters().getInfectionRadius();
+        return Math.ceilDiv(ticks, incubationTime) * radius;
     }
 
     @Override
     public Output getOutput() {
-        throw new RuntimeException("not implemented");
+        return new Output(this.scenario, totalTrace, this.totalStatistics);
     }
 
+    /**
+     * Runs the simulation.
+     * Starts all patches' threads.
+     * Collects patches' output while they simulate.
+     */
     @Override
     public void run() {
-        throw new RuntimeException("not implemented");
+        for (Patch patch : patches) {
+            Thread thread = new Thread(patch);
+            threads.add(thread);
+            thread.start();
+        }
+
+        collectOutput();
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void populate() {
+        // we populate the context with persons based on the respective info objects.
+        // As context null is provided since patches will reassign context themselves.
+        int id = 0;
+        for (PersonInfo personInfo : this.scenario.getPopulation()) {
+            this.initialPopulation.add(new Person(id, null, this.scenario.getParameters(), personInfo));
+            id++;
+        }
+    }
+
+    /**
+     * Iterates over patches as defined in the scenario and creates associated Patch objects.
+     * Stores patch objects in the list of patches.
+     * For each patch creates a separate output queue that patch can write output to after every
+     * tick.
+     * Stores the queue into a list of queues to later retrieve outputs.
+     */
+    private void createPatches() {
+        Iterator<Rectangle> patchesIterator = Utils.getPatches(scenario);
+
+        while (patchesIterator.hasNext()) {
+            Rectangle patchArea = patchesIterator.next();
+            Rectangle paddedArea = Utils.getPaddedArea(padding, patchArea, scenario.getGrid());
+            MonitorQueue<OutputEntry> outputQueueForPatch = new MonitorQueue<>();
+            outputQueues.add(outputQueueForPatch);
+
+            List<Person> patchPopulation = initialPopulation.stream().filter(person -> patchArea.contains(person.getPosition())).toList();
+            patches.add(
+                    new Patch(patchPopulation,
+                            patchArea,
+                            paddedArea,
+                            cycleDuration,
+                            scenario,
+                            patches.size(),
+                            validator,
+                            outputQueueForPatch
+                    )
+            );
+        }
+    }
+
+    /**
+     * Iterates over patches created via createPatches() and initializes for each of them
+     * associated paddings intersected with other patches.
+     * Stores created PatchBuffers representing pieces of geometric padding into correspoding
+     * innerPaddings or outerPaddings of the appropriate patch.
+     */
+    private void createPaddings() {
+        for (Patch outerPatch : patches) {
+
+            for (Patch innerPatch : patches) {
+                if (innerPatch == outerPatch) continue;
+                if (!innerPatch.getPatchArea().overlaps(outerPatch.getPaddedArea())) continue;
+
+                Rectangle intersectionOfPaddingAndPatch =
+                        outerPatch.getPaddedArea().intersect(innerPatch.getPatchArea());
+
+                if (!gridManager.mayPropagateFrom(scenario,
+                        intersectionOfPaddingAndPatch, outerPatch.getPatchArea())) continue;
+
+                PaddingBuffer paddingBuffer =
+                        new PaddingBuffer(intersectionOfPaddingAndPatch);
+
+                innerPatch.addInnerPadding(paddingBuffer);
+                outerPatch.addOuterPadding(paddingBuffer);
+            }
+        }
+
+    }
+
+    /**
+     * Fills a statistics list with an empty Statistics object to later merge them with actual
+     * statistics retrieved from the patches.
+     */
+    private void initializeStatistics() {
+        for (String key : this.scenario.getQueries().keySet()) {
+            List<Statistics> initializedArray = new ArrayList<>(statsLength);
+            for (int i = 0; i < statsLength; i++) {
+                initializedArray.add(new Statistics(0, 0, 0, 0));
+            }
+
+            totalStatistics.put(key, initializedArray);
+        }
+    }
+
+    /**
+     * Collects output from output queues and merges them into final output.
+     */
+    private void collectOutput() {
+
+        for (int i = 0; i < statsLength; i++) {
+            List<PersonInfoWithId> trace = new ArrayList<>();
+
+            for (MonitorQueue<OutputEntry> outputQueue : outputQueues) {
+                OutputEntry entry = outputQueue.dequeue();
+
+                // Merge statistics
+                collectPatchStatistics(entry.statisticsForTick(), entry.tick());
+
+                // If trace disabled - skip
+                if (!scenario.getTrace()) continue;
+
+                // Process traces
+
+                //  Merge sorted traces
+                trace = Utils.merge(
+                        trace,
+                        entry.traceForTick(),
+                        Comparator.comparing(PersonInfoWithId::id)
+                );
+            }
+
+            // If trace disabled - skip
+            if (!scenario.getTrace()) continue;
+
+            TraceEntry traceEntry = new TraceEntry(trace.stream().map(PersonInfoWithId::personInfo).toList());
+            totalTrace.add(traceEntry);
+        }
+    }
+
+    /**
+     * Updates totalStatistics with provided statistic.
+     *
+     * @param patchStatsMap statistics from some patch to merge with totalStatistics.
+     * @param tick          Tick for which statistics is provided.
+     */
+    private void collectPatchStatistics(Map<String, Statistics> patchStatsMap, int tick) {
+        for (String key : patchStatsMap.keySet()) {
+            Statistics mergedStatistics = Utils.mergeStatistics(
+                    totalStatistics.get(key).get(tick), patchStatsMap.get(key)
+            );
+            totalStatistics.get(key).set(tick, mergedStatistics);
+        }
     }
 }
